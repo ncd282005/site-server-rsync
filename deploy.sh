23017cbd643e6dbad323cade04b9f09042c830be
ncd282005 <ncd635473@gmail.com>
2026-04-03 14:58:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-SERVER_IP="3.83.3.138"
+SERVER_IP="-----------"
 USER="ubuntu"
-KEY="/home/pc/.ssh/private_key.pem"
+KEY="/home/key"
 LOCAL_DIR="/var/www/html/mysite/"
 REMOTE_DIR="/var/www/html/"
 
 echo " Deploying website..."
 
-rsync -avz -e "ssh -i private_key.pem -o IdentitiesOnly=yes" /var/www/html/ ubuntu@3.83.3.138:/var/www/html
+rsync -avz -e "ssh -i key -o IdentitiesOnly=yes" /var/www/html/ ubuntu@server-ip:/var/www/html
 
 if [ $? -eq 0 ]; then
     echo " Deployment successful!"
